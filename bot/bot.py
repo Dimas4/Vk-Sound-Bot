6d40b320bc540775fa70818438709ec2ab6a6de2
Ivan <ivanshatukho@yandex.ru>
2018-10-11 08:54:47 +0300

    try except

--- bot/bot.py
+++ bot/bot.py
@@ -13,13 +13,16 @@ def start(token, backend, db, filename):
     db = Db(db_path)
 
     while True:
-        messages = bot.get_unread_messages()
-        if messages["count"] >= 1:
-            id, body = bot.get_message_and_id(messages)
-            bot.convert_text_to_voice(body)
-            uploaded_voice = bot.upload_file(filename, id)
-            bot.send_message(id, attach=uploaded_voice)
+        try:
+            messages = bot.get_unread_messages()
+            if messages["count"] >= 1:
+                id, body = bot.get_message_and_id(messages)
+                bot.convert_text_to_voice(body)
+                uploaded_voice = bot.upload_file(filename, id)
+                bot.send_message(id, attach=uploaded_voice)
 
-            db.rpush(id, body)
+                db.rpush(id, body)
 
-        time.sleep(1)
+            time.sleep(1)
+        except Exception as e:
+            print('Error:', str(e))
